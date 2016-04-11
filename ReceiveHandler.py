f0f625f4a00823234d99150cfabda97da871d84a
Riccardos <riccardo.stefani91@gmail.com>
2016-04-11 16:16:35 +0200

    Aggiunti File

---
+++ ReceiveHandler.py
@@ -0,0 +1,245 @@
+from Utility import *
+from Communication import *
+from Parser import *
+import asyncore
+import logging
+import os
+
+class ReceiveHandler(asyncore.dispatcher):
+
+    def __init__(self, conn_sock, address):
+        asyncore.dispatcher.__init__(self,conn_sock)
+        self.conn_sock = conn_sock
+        self.address = address
+        self.out_buffer = []
+
+##  metodo di scrittura sul buffer, quando pronto viene svuotato
+##  molto utile per l'upload
+    def write(self, data):
+        self.out_buffer.append(data)
+
+##  eventuale metodo per chiudere la connessione
+##  questo va utilizzato se si vuole evitare di ricevere 0
+    def shutdown(self):
+        self.out_buffer.append(None)
+
+##  overide metodo handle_read
+    def handle_read(self):
+
+        data = self.recv(2048)
+        logging.debug(data)
+
+        if len(data) > 0:
+            # converto i comandi
+            command, fields = Parser.parse(data.decode())
+
+            if command == "RETR":
+                # TODO controllare coerenza con nuovi metodi
+                # Imposto la lunghezza dei chunk e ottengo il nome del file a cui corrisponde l'md5
+                chuncklen = 512
+                peer_md5 = fields[0]
+                # TODO cambiato questo metodo perche il database e cambiato
+                obj = Utility.database.findFile(Utility.sessionId,peer_md5)
+
+                if len(obj) > 0:
+                    # svuota il buffer
+                    self.out_buffer = []
+                    filename = Utility.PATHDIR + str(obj[0][0])
+                    # lettura statistiche file
+                    statinfo = os.stat(filename)
+                    # imposto lunghezza del file
+                    len_file = statinfo.st_size
+                    # controllo quante parti va diviso il file
+                    num_chunk = len_file // chuncklen
+                    if len_file % chuncklen != 0:
+                        num_chunk = num_chunk + 1
+                    # pad con 0 davanti
+                    num_chunk = str(num_chunk).zfill(6)
+                    # costruzione risposta come ARET0000XX
+                    mess = ('ARET' + num_chunk).encode()
+                    self.write(mess)
+
+                    # Apro il file in lettura e ne leggo una parte
+                    f = open(filename, 'rb')
+                    r = f.read(chuncklen)
+
+                    # Finchè il file non termina
+                    while len(r) > 0:
+
+                        # Invio la lunghezza del chunk
+                        mess = str(len(r)).zfill(5).encode()
+                        self.write(mess + r)
+                        logging.debug('messaggio nel buffer pronto')
+
+                        # Proseguo la lettura del file
+                        r = f.read(chuncklen)
+                    # Chiudo il file
+                    f.close()
+                    self.shutdown()
+
+            elif command == "QUER":
+                # TODO implementare metodo QUER
+                True
+
+            elif command=="AQUE":
+                # TODO implementare metodo ricezione AQUE
+                True
+
+            #Procedura LOGI
+            elif command=='LOGI':
+                # solo il supernodo risponde a una LOGI
+                if Utility.superNodo:
+                    ip=fields[0]
+                    port=fields[1]
+                    try:
+                        # se il peer è presente gli do il suo vecchio sessionId altrimenti uno nuovo
+                        l=Utility.database.findPeer('',ip,port,1)
+                        if len(l)>0:
+                            ssID=l[0][0]
+                        else:
+                            ssID=Utility.generateId(16)
+                        Utility.database.addPeer(ssID,ip,port)
+                    except Exception as e:
+                        ssID='0'*16
+
+                    msgRet='ALGI'+ssID
+                    t=Sender(msgRet,ip,port)
+                    t.start()
+
+            # Procedura ALGI
+            elif command=='ALGI':
+                # Solo il peer deve elaborare una algi
+                if not Utility.superNodo:
+                    # controllo se ho ricevuto un sessionId valido se si lo salvo altrimenti no
+                    s='0'*16
+                    ssID=fields[0]
+                    if ssID==s:
+                        Utility.ipSuperNodo=''
+                        Utility.portSuperNodo=''
+                    else:
+                        Utility.sessionId=ssID
+
+            #Procedura ADFF
+            elif command=='ADFF':
+                # solo il supernodo deve elaborare una adff
+                if Utility.superNodo:
+                    ssID=fields[0]
+                    md5=fields[1]
+                    name=fields[2]
+                    # controllo se il sessionId e registrato nel database
+                    # se si aggiungo il file al database
+                    l=Utility.database.findPeer(ssID,'','',2)
+                    if len(l)>0:
+                        Utility.database.addFile(ssID,name,md5)
+
+            # Procedura DEFF
+            elif command=='DEFF':
+                # solo il supernodo deve elaborare una deff
+                if Utility.superNodo:
+                    ssID=fields[0]
+                    md5=fields[1]
+                    # controllo se il sessionId e registrato nel database
+                    # se si rimuovo il file al database
+                    l=Utility.database.findPeer(ssID,'','',2)
+                    if len(l)>0:
+                        Utility.database.removeFile(ssID,md5)
+
+            # Procedura LOGO
+            elif command=='LOGO':
+                # solo il supernodo deve elaborare una richiesta logo
+                if Utility.superNodo:
+                    ssID=fields[0]
+                    # controllo se il sessionId e nel database
+                    l=Utility.database.findPeer(ssID,'','',2)
+                    if len(l)>0:
+                        # se il sessionId e presente rimuovo i suoi file e ritorno il messaggio ALGO
+                        ip=l[0][0]
+                        port=l[0][1]
+                        canc=Utility.database.removeAllFileForSessionId(ssID)
+                        msgRet='ALGO'+'{:0>3}'.format(canc)
+                        t=Sender(msgRet,ip,port)
+                        t.start()
+
+            # Procedura ALGO
+            elif command=='ALGO':
+                # solo il peer deve elaborare la ALGO
+                if not Utility.superNodo:
+                    #Azzero le variabili e stampo
+                    delete=fields[0]
+                    Utility.sessionId=''
+                    Utility.ipSuperNodo=''
+                    Utility.portSuperNodo=''
+                    print('Logout effetuato, cancellati: '+delete)
+
+            # Gestisco arrivo pacchetto supe
+            elif command=="SUPE":
+                pkID=fields[0]
+                if Utility.database.checkPkt(pkID)==False:
+                    Utility.database.addPkt(pkID)
+                    # Se sono un supernodo rispondo con asup
+                    if Utility.superNodo:
+                        ip=Utility.MY_IPV4+"|"+Utility.MY_IPV6
+                        port='{:0>5}'.format(Utility.PORT)
+                        msgRet="ASUP"+pkID+ip+port
+                        t=Sender(msgRet,fields[1],fields[2])
+                        t.start()
+                    # Decremento il ttl e controllo se devo inviare
+                    ttl = int(fields[3])-1
+                    if ttl > 0:
+                        ttl='{:0>2}'.format(ttl)
+                        msg="SUPE"+pkID+fields[1]+fields[2]+ttl
+                        listaP=Utility.database.listPeer()
+                        if len(listaP)>0:
+                            tP = SenderAll(msg,listaP)
+                            tP.run()
+                        listaS=Utility.database.listSuperNode()
+                        if len(listaS)>0:
+                            tS = SenderAll(msg,listaS)
+                            tS.run()
+
+            elif command=="ASUP":
+                pkID=fields[0]
+                ip=fields[1]
+                port=fields[2]
+                if Utility.superNodo==True and Utility.database.checkPkt(pkID)==True:
+                    Utility.database.addSuperNode(ip,port)
+                else:
+                    findPeer=False
+                    for i in range(0,len(Utility.listFindSNode)):
+                        if Utility.listFindSNode[i][1]==ip and Utility.listFindSNode[i][2]==port:
+                            findPeer=True
+
+                    if Utility.database.checkPkt(pkID)==True and findPeer:
+                        global numFindSNode
+                        numFindSNode+=1
+                        Utility.listFindSNode.append(fields)
+                        print(str(Utility.numFindSNode) + " " + ip + " " + port)
+
+            else:
+                logging.debug('ricevuto altro')
+
+##   metodo che verifica la possibilita' di scrivere delle
+##   informazioni sul canale
+    def writable(self):
+        return (len(self.out_buffer) > 0)
+
+##  metodo per scrivere in uscita dal canale
+    def handle_write(self):
+        if self.out_buffer[0] is None:
+            logging.debug('informazione trasferita')
+            self.close()
+            return
+
+        sent = self.send(self.out_buffer[0])
+        logging.debug('sto svuotando il buffer')
+        # sistemare eventuale errore NoneType su out_buffer
+        if sent >= len(self.out_buffer[0]):
+            self.out_buffer.pop(0)
+        else:
+            self.out_buffer[0] = self.out_buffer[0][sent:]
+
+##  metodo per chiudere il canale
+    def handle_close(self):
+        logging.debug('sto uscendo dal canale')
+        self.close()
+
---
+++ Server.py
@@ -0,0 +1,75 @@
+from ReceiveHandler import *
+from Utility import *
+import logging
+import threading
+import queue
+import asyncore
+import socket
+
+class Server_Peer:
+
+    def __init__(self,ipv4,ipv6):
+        self.ipv4=ipv4
+        self.ipv6=ipv6
+        self.port=Utility.PORT                        # da sostituire con Utility.generatePort()
+        self.stop_queue = queue.Queue(1)
+        u1 = ReceiveServerIPV4(self.stop_queue,self.ipv4,self.port)
+        self.server_thread = threading.Thread(target=u1)#crea un thread e gli assa l'handler per il server da far partire
+        self.stop_queueIpv6 = queue.Queue(1)
+        u2 = ReceiveServerIPV6(self.stop_queueIpv6,self.ipv6,self.port)
+        self.server_threadIP6 = threading.Thread(target=u2)
+        self.server_thread.start()#parte
+        self.server_threadIP6.start()
+
+
+class ReceiveServerIPV4(asyncore.dispatcher):
+    """Questa classe rappresenta un server per accettare i pacchetti
+    degli altri peer."""
+    def __init__(self, squeue, ip, port):
+        asyncore.dispatcher.__init__(self)
+        self.squeue = squeue
+        self.create_socket(socket.AF_INET,socket.SOCK_STREAM)#crea socket ipv6
+        self.set_reuse_addr()#riusa indirizzo, evita problemi indirizzo occupato
+        self.bind((ip, port)) #crea la bind del mio ip e porta
+        self.listen(5)# sta in ascolto di 5 persone max
+
+    # definizione dell'accept delle connessioni in arrivo al server
+    def handle_accepted(self, client, address):
+        # client_info = client, address
+        logging.debug('handle_accept() %s', address)
+        ReceiveHandler(client, address)
+
+    def __call__(self):
+        try:
+            asyncore.loop()
+        except Exception as e:
+            logging.debug(e)
+
+    def handle_close(self):
+        self.close()
+
+class ReceiveServerIPV6(asyncore.dispatcher):
+    """Questa classe rappresenta un server per accettare i pacchetti
+    degli altri peer."""
+    def __init__(self, squeue, ip, port):
+        asyncore.dispatcher.__init__(self)
+        self.squeue = squeue
+        self.create_socket(socket.AF_INET6,socket.SOCK_STREAM)#crea socket ipv6
+        self.set_reuse_addr()#riusa indirizzo, evita problemi indirizzo occupato
+        self.bind((ip, port)) #crea la bind del mio ip e porta
+        self.listen(5)# sta in ascolto di 5 persone max
+
+    # definizione dell'accept delle connessioni in arrivo al server
+    def handle_accepted(self, client, address):
+        # client_info = client, address
+        logging.debug('handle_accept() %s', address)
+        ReceiveHandler(client, address)
+
+    def __call__(self):
+        try:
+            asyncore.loop()
+        except Exception as e:
+            logging.debug(e)
+
+    def handle_close(self):
+        self.close()
